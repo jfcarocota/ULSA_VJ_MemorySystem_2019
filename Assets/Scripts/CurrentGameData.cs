ae7b930d0404918cae09730a44e71b74c19fecbf
jfcarocota <jfcarocota@gmail.com>
2019-02-15 14:24:19 -0700

    scriptable objects working

---
+++ Assets/Scripts/CurrentGameData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Game Data", menuName = "Custom Objects/Game Data")]
+class CurrentGameData : ScriptableObject
+{
+    [SerializeField]
+    GameData gameData;
+
+    internal GameData GameData { get => gameData; set => gameData = value; }
+}
+
